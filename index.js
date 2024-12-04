f5de4a0cc5a78cf0445643430928d1c959ed7eb6
unknown <malikhassanhu55@gmail.com>
2024-12-04 20:20:02 +0500

    ds

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js on Vercel!');
+});
+
+app.get("/login",(req,res)=>{
+    res.send("Noman Hassan")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
